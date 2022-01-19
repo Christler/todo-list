ebb521f8bd7a3b3b653025a77f60de6493957965
Chris Ear <christear85@gmail.com>
2022-01-19 15:49:37 -0700

    added dummy data to UI.js

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -7,5 +7,42 @@ export default class UI {
     static loadPage(){
         const content = document.querySelector('.main')
         content.innerHTML = `<h1>Hello World</h1>`
+        let data = UI.loadData()
+        let projects = data.projects
+        UI.displayProjects(projects)
+    }
+
+    static loadData(){
+        //dummy data
+        const projectList = new ProjectList()
+
+        const project1 = new Project('Grocery Store')
+        project1.addTodo(new Todo('Milk'))
+        project1.addTodo(new Todo('Cheese'))
+        project1.addTodo(new Todo('Bread'))
+        project1.addTodo(new Todo('Beer'))
+
+        const project2 = new Project('Home Project')
+        project2.addTodo(new Todo('Paint Cabinets'))
+        project2.addTodo(new Todo('Tile Backsplash'))
+        project2.addTodo(new Todo('New Lighting'))
+
+        const project3 = new Project('Workout')
+        project3.addTodo(new Todo('Chest'))
+        project3.addTodo(new Todo('Back'))
+        project3.addTodo(new Todo('Legs'))
+        project3.addTodo(new Todo('Arms'))
+
+        projectList.addProject(project1)
+        projectList.addProject(project2)
+        projectList.addProject(project3)
+
+        return projectList
+    }
+
+    static displayProjects(projects){
+        projects.forEach(project => {
+            console.log(project.name)
+        });
     }
 }
