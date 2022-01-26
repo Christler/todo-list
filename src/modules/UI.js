40269c4adbfe03ffb5264af4e5ebc4591e1d12a6
Chris Ear <christear85@gmail.com>
2022-01-26 07:09:58 -0700

    hid new todo button until a project is selected

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,7 +4,7 @@ import TempData from "./TempDataStore"
 
 export default class UI {
 
-    static loadPage(){
+    static loadPage() {
 
         //get data from temp data store
         const data = TempData()
@@ -12,22 +12,22 @@ export default class UI {
         createEventListeners()
         let selectedProject
 
-        function createEventListeners(){
+        function createEventListeners() {
             const newProjectBtn = document.querySelector('.newProjectBtn')
             newProjectBtn.addEventListener('click', addProject)
 
             const newTodoBtn = document.querySelector('.newTodoBtn')
             newTodoBtn.addEventListener('click', addTodo)
         }
-        
-        function displayProjects(){
-            
+
+        function displayProjects() {
+
             //clear project section before displaying
             const projectsSection = document.querySelector('.projects')
             projectsSection.textContent = ''
-            
+
             let projects = data.projects
-            
+
             projects.forEach((project, index) => {
                 let element = document.createElement('p')
                 element.className = 'project'
@@ -39,12 +39,17 @@ export default class UI {
             })
         }
 
-        function displayTodos(index){
+        function displayTodos(index) {
             
+            if(!selectedProject){
+                const newTodoSection = document.querySelector('.newTodoSection')
+                newTodoSection.style.display = 'block'
+            }
+
             //clear main before showing todos
             const todoSection = document.querySelector('.todos')
             todoSection.textContent = ''
-    
+
             //get project by using index of element that was clicked
             selectedProject = data.projects[index]
 
@@ -57,20 +62,24 @@ export default class UI {
                 todoDiv.className = 'todo'
                 todoDiv.textContent = todo.name
                 todoSection.append(todoDiv)
-            })  
+            })
         }
 
-        function addProject(){
+        function addProject() {
             let name = prompt("Enter project name")
             data.projects.push(new Project(name))
             displayProjects()
         }
 
-        function addTodo(){
-            let name = prompt("Enter todo name")
-            selectedProject.addTodo(new Todo(name))
-            let index = data.projects.findIndex(p => p === selectedProject)
-            displayTodos(index)
+        function addTodo() {
+            if (selectedProject) {
+                let name = prompt("Enter todo name")
+                selectedProject.addTodo(new Todo(name))
+                let index = data.projects.findIndex(p => p === selectedProject)
+                displayTodos(index)
+            }else{
+                alert("Select a existing Project or create a new one.")
+            }
         }
     }
 }
