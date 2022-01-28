f06dd9a8056523fb7357623567c8b22e3b588981
Chris Ear <christear85@gmail.com>
2022-01-28 16:20:47 -0700

    moved display todo logic to project class

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -12,4 +12,46 @@ export default class Project{
     deleteTodo(index){
         this.todos.splice(index, 1)
     }
+
+    displayTodos(){
+        const todoSection = document.querySelector('.todos')
+            todoSection.textContent = ''
+
+            const projectName = document.createElement('h1')
+            projectName.textContent = this.name
+            todoSection.append(projectName)
+
+            this.todos.forEach((todo, index) => {
+                const todoDiv = document.createElement('div')
+                todoDiv.className = 'todo'
+                
+                const todoCheckbox = document.createElement('input')
+                todoCheckbox.setAttribute('type', 'checkbox')
+                todoCheckbox.checked = todo.getCompleteStatus()
+                todoCheckbox.addEventListener('click', () => {
+                    todo.setCompleteStatus()
+                })
+                
+                const todoName = document.createElement('div')
+                todoName.textContent = todo.name
+
+                const datePicker = document.createElement('input')
+                datePicker.setAttribute('type', 'date')
+                datePicker.value = todo.getDueDate()
+                datePicker.addEventListener('change', (e) => {
+                    todo.setDueDate(e.target.value)
+                })
+                
+                const deleteBtn = document.createElement('button')
+                deleteBtn.className = 'deleteBtn'
+                deleteBtn.textContent = 'X'
+                deleteBtn.addEventListener('click', () => {
+                    this.deleteTodo(index)
+                    this.displayTodos()
+                })
+                
+                todoDiv.append(todoCheckbox, todoName, datePicker, deleteBtn)
+                todoSection.append(todoDiv)
+            })
+    }
 }
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -45,47 +45,8 @@ export default class UI {
                 newTodoSection.style.display = 'block'
             }
 
-            const todoSection = document.querySelector('.todos')
-            todoSection.textContent = ''
-
             selectedProject = data.projects[index]
-
-            const projectName = document.createElement('h1')
-            projectName.textContent = selectedProject.name
-            todoSection.append(projectName)
-
-            selectedProject.todos.forEach((todo, index) => {
-                const todoDiv = document.createElement('div')
-                todoDiv.className = 'todo'
-                todoDiv.dataset.indexNumber = index
-                
-                const todoCheckbox = document.createElement('input')
-                todoCheckbox.setAttribute('type', 'checkbox')
-                todoCheckbox.checked = todo.getCompleteStatus()
-                todoCheckbox.addEventListener('click', () => {
-                    markComplete(index)
-                })
-                
-                const todoName = document.createElement('div')
-                todoName.textContent = todo.name
-
-                const datePicker = document.createElement('input')
-                datePicker.setAttribute('type', 'date')
-                datePicker.value = todo.getDueDate()
-                datePicker.addEventListener('change', (e) => {
-                    todo.setDueDate(e.target.value)
-                })
-                
-                const deleteBtn = document.createElement('button')
-                deleteBtn.className = 'deleteBtn'
-                deleteBtn.textContent = 'X'
-                deleteBtn.addEventListener('click', () => {
-                    deleteTodo(index)
-                })
-                
-                todoDiv.append(todoCheckbox, todoName, datePicker, deleteBtn)
-                todoSection.append(todoDiv)
-            })
+            selectedProject.displayTodos()
         }
 
         function addProject() {
@@ -98,15 +59,6 @@ export default class UI {
             let name = prompt("Enter todo name")
             selectedProject.addTodo(new Todo(name))
             displayTodos(data.getProjectIndex(selectedProject))
-        }
-
-        function deleteTodo(index){
-            selectedProject.deleteTodo(index)
-            displayTodos(data.getProjectIndex(selectedProject))
-        }
-
-        function markComplete(index){
-            selectedProject.todos[index].setCompleteStatus()
         }
     }
 }
