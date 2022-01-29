b503cdbdf6d8de90cffc9823df134d02cd2f81b8
Chris Ear <christear85@gmail.com>
2022-01-28 17:03:10 -0700

    added calendar icon to cleanup todo

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,57 +1,68 @@
-export default class Project{
+export default class Project {
 
-    constructor(name){
+    constructor(name) {
         this.name = name
         this.todos = []
     }
 
-    addTodo(todo){
+    addTodo(todo) {
         this.todos.push(todo)
     }
 
-    deleteTodo(index){
+    deleteTodo(index) {
         this.todos.splice(index, 1)
     }
 
-    displayTodos(){
+    displayTodos() {
         const todoSection = document.querySelector('.todos')
-            todoSection.textContent = ''
+        todoSection.textContent = ''
 
-            const projectName = document.createElement('h1')
-            projectName.textContent = this.name
-            todoSection.append(projectName)
+        const projectName = document.createElement('h1')
+        projectName.textContent = this.name
+        todoSection.append(projectName)
 
-            this.todos.forEach((todo, index) => {
-                const todoDiv = document.createElement('div')
-                todoDiv.className = 'todo'
-                
-                const todoCheckbox = document.createElement('input')
-                todoCheckbox.setAttribute('type', 'checkbox')
-                todoCheckbox.checked = todo.getCompleteStatus()
-                todoCheckbox.addEventListener('click', () => {
-                    todo.setCompleteStatus()
-                })
-                
-                const todoName = document.createElement('div')
-                todoName.textContent = todo.name
+        this.todos.forEach((todo, index) => {
+            const todoDiv = document.createElement('div')
+            todoDiv.className = 'todo'
 
-                const datePicker = document.createElement('input')
-                datePicker.setAttribute('type', 'date')
-                datePicker.value = todo.getDueDate()
-                datePicker.addEventListener('change', (e) => {
-                    todo.setDueDate(e.target.value)
-                })
-                
-                const deleteBtn = document.createElement('button')
-                deleteBtn.className = 'deleteBtn'
-                deleteBtn.textContent = 'X'
-                deleteBtn.addEventListener('click', () => {
-                    this.deleteTodo(index)
-                    this.displayTodos()
-                })
-                
-                todoDiv.append(todoCheckbox, todoName, datePicker, deleteBtn)
-                todoSection.append(todoDiv)
+            const todoCheckbox = document.createElement('input')
+            todoCheckbox.setAttribute('type', 'checkbox')
+            todoCheckbox.checked = todo.getCompleteStatus()
+            todoCheckbox.addEventListener('click', () => {
+                todo.setCompleteStatus()
             })
+
+            const todoName = document.createElement('div')
+            todoName.textContent = todo.name
+
+            const dueDate = document.createElement('p')
+            dueDate.textContent = todo.getDueDate()
+
+            const datePicker = document.createElement('input')
+            datePicker.setAttribute('type', 'date')
+            datePicker.value = todo.getDueDate()
+            datePicker.addEventListener('change', (e) => {
+                todo.setDueDate(e.target.value)
+                this.displayTodos()
+            })
+            datePicker.className = 'hidden'
+
+            const calendarIcon = document.createElement('i')
+            calendarIcon.className = 'fas fa-calendar'
+            calendarIcon.addEventListener('click', () =>{
+                datePicker.classList.toggle('hidden')
+            })
+
+            const deleteBtn = document.createElement('button')
+            deleteBtn.className = 'deleteBtn'
+            deleteBtn.textContent = 'X'
+            deleteBtn.addEventListener('click', () => {
+                this.deleteTodo(index)
+                this.displayTodos()
+            })
+
+            todoDiv.append(todoCheckbox, todoName, dueDate, datePicker, calendarIcon, deleteBtn)
+            todoSection.append(todoDiv)
+        })
     }
 }
